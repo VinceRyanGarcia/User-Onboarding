36839b0915a7f4a07dbf4c8d56ba44d770f00c50
Vince Garcia <vinceryangarcia@gmail.com>
2021-01-28 13:47:15 -0800

    initial push

---
+++ user-onboarding/src/validation/formSchema.js
@@ -0,0 +1,21 @@
+
+import * as yup from "yup";
+
+
+export default yup.object().shape({
+    name: yup
+    .string()
+    .required("Name is required")
+    .min(4, "Name must be at least 4 characters long"),
+    email: yup
+    .string()
+    .email("Your email, Peon")
+    .required("email is required"),
+    password: yup
+    .string()
+    .required("Password is required"),
+    terms: yup
+    .boolean()
+    .oneOf([true], 'Terms of service must be agreed on')
+})
+
